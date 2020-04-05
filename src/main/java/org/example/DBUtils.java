c6f3ccc4f854029120cb231165672195aaa24763
Igor Mokritsky <igormokritsky@gmail.com>
2020-04-05 14:03:12 +0300

    update

--- src/main/java/org/example/DBUtils.java
+++ src/main/java/org/example/DBUtils.java
@@ -5,9 +5,9 @@ import java.sql.SQLException;
 
 
 public class DBUtils {
-    private static String userName = "root";
-    private static String password = "Jummetmokai2145";
-    private static String connectionURL = "jdbc:mysql://localhost:3306/swimmers_schema";
+    private static final String userName = "root";
+    private static final String password = "Jummetmokai2145";
+    private static final String connectionURL = "jdbc:mysql://localhost:3306/swimmers_schema";
 
     public static Connection getConnection(){
         Connection connection = null;
@@ -16,25 +16,25 @@ public class DBUtils {
             connection = DriverManager.getConnection(connectionURL, userName, password);
 
         } catch (SQLException e) {
-            printSQLException(e);
+            throw new ServiceException(e.getMessage(), e);
         }
         return connection;
     }
 
-    public static void printSQLException(SQLException ex) {
-        for (Throwable e: ex) {
-            if (e instanceof SQLException) {
-                e.printStackTrace(System.err);
-                System.err.println("SQLState: " + ((SQLException) e).getSQLState());
-                System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
-                System.err.println("Message: " + e.getMessage());
-                Throwable t = ex.getCause();
-                while (t != null) {
-                    System.out.println("Cause: " + t);
-                    t = t.getCause();
-                }
-            }
-        }
-    }
+//    public static void printSQLException(SQLException ex) {
+//        for (Throwable e: ex) {
+//            if (e instanceof SQLException) {
+//                e.printStackTrace(System.err);
+//                System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+//                System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+//                System.err.println("Message: " + e.getMessage());
+//                Throwable t = ex.getCause();
+//                while (t != null) {
+//                    System.out.println("Cause: " + t);
+//                    t = t.getCause();
+//                }
+//            }
+//        }
+//    }
 
 }
